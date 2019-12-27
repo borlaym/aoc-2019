1631e32cb1e80583187ddcb31a7a8977d3a38b1f
Marton Borlay <borlaym@gmail.com>
2019-12-27 23:44:39 +0100

    Initial commit

---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,19 @@
+package com.borlaym.aoc
+import scala.io.Source
+
+object Day1 {
+	def main(args: Array[String]): Unit = {
+		val sources = Source.fromFile("src/day1Source.txt").getLines.toList.map(_.toInt)
+		val sum = sources.map(getFuelRequirement(_)).foldLeft(0)((a: Int, b: Int) => a + b)
+		println(sum)
+	}
+
+	def getFuelRequirement(mass: Int): Int = {
+		val required = mass / 3 - 2
+		if (required < 0) {
+			0
+		} else {
+			required + getFuelRequirement(required)
+		}
+	}
+}
---
+++ src/test/scala/Day1Test.scala
@@ -0,0 +1,10 @@
+import com.borlaym.aoc.Day1
+import org.scalatest.FunSuite
+
+class Day1Test extends FunSuite {
+	test("Day1.getFuelRequirement") {
+		assert(Day1.getFuelRequirement(12) === 2)
+		assert(Day1.getFuelRequirement(1969) === 966)
+		assert(Day1.getFuelRequirement(100756) === 50346)
+	}
+}
