0810747a681816af11263b5952908931a0e67d11
Marton Borlay <borlaym@gmail.com>
2019-12-29 11:27:07 +0100

    Day6 Part 2

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -1,6 +1,22 @@
 import scala.io.Source
 
 object Day6 {
+	def buildChain(allOrbits: Map[String, String], name: String, chain: List[String]): List[String] = {
+		allOrbits.get(name) match {
+			case None => chain
+			case Some(around) => buildChain(allOrbits, around, around :: chain)
+		}
+	}
+	def sharedSlice(list1: List[String], list2: List[String], shared: List[String]): List[String] = {
+		list1 match {
+			case head :: _ if list2.head == head => sharedSlice(list1.tail, list2.tail, shared :+ head)
+			case _ => shared
+		}
+	}
+	def distance(from: List[String], to: List[String]): Int = {
+		val shared = sharedSlice(from, to, List())
+		from.drop(shared.length).length + to.drop(shared.length).length
+	}
 	def singleOrbit(allOrbits: Map[String, String], name: String): Int = {
 		allOrbits.get(name) match {
 			case None => 0
@@ -21,5 +37,9 @@ object Day6 {
 		}.toMap
 		val orbits = calculateOrbits(sources)
 		println(orbits)
+
+		val santaChain = buildChain(sources, "SAN", List())
+		val youChain = buildChain(sources, "YOU", List())
+		println(distance(santaChain, youChain))
 	}
 }
--- src/test/scala/Day6Test.scala
+++ src/test/scala/Day6Test.scala
@@ -12,11 +12,24 @@ class Day6Test extends FunSuite {
 		"I" -> "D",
 		"J" -> "E",
 		"K" -> "J",
-		"L" -> "K"
+		"L" -> "K",
+		"YOU" -> "K",
+		"SAN" -> "I"
 	)
 	test("Day6.singleOrbit") {
 		assert(Day6.singleOrbit(orbits, "D") == 3)
 		assert(Day6.singleOrbit(orbits, "L") == 7)
-		assert(Day6.calculateOrbits(orbits) == 42)
+		assert(Day6.calculateOrbits(orbits) == 54)
+	}
+	test("Day6.buildChain") {
+		assert(Day6.buildChain(orbits, "YOU", List()) == List("COM", "B", "C", "D", "E", "J", "K"))
+	}
+	test("Day6.sharedSlice") {
+		assert(Day6.sharedSlice(List("A", "B", "D"), List("A", "B", "C"), List()) == List("A", "B"))
+	}
+	test("Day6.distance") {
+		val santa = Day6.buildChain(orbits, "SAN", List())
+		val you = Day6.buildChain(orbits, "YOU", List())
+		assert(Day6.distance(santa, you) == 4)
 	}
 }
