0e58076307e808795529f85fad19cf0eb8889086
Marton Borlay <borlaym@gmail.com>
2019-12-29 22:58:08 +0100

    Day 9 Part 2

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -3,7 +3,7 @@ import scala.io.Source
 object Day9 {
 	def main(args: Array[String]): Unit = {
 		val program = Source.fromFile("src/day9Source.txt").getLines().toList.map(_.split(",")).flatten.map(BigInt(_))
-		val results = IntCode2.execute(program, List(BigInt(1)))
+		val results = IntCode2.execute(program, List(BigInt(2)))
 		println(results)
 	}
 }
