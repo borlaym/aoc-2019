2b02f053da2b416686ad4ee18494bee38f2889fa
Marton Borlay <borlaym@gmail.com>
2019-12-29 13:27:38 +0100

    Day8 Part 2

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -6,10 +6,23 @@ object Day8 {
 		imageData.grouped(layerLength).toList
 	}
 
+	def applyLayers(imageData: List[Int], width: Int, height: Int): List[Int] = {
+		val allLayers = layers(imageData, width, height)
+		allLayers.init.foldRight(allLayers.last)((layer, image) => {
+			(image zip layer).map {
+				case (below, 2) => below
+				case (_, above) => above
+			}
+		})
+	}
+
 	def main(args: Array[String]): Unit = {
 		val imageData = Source.fromFile("src/day8Source.txt").toList.map(_.toString).map(_.toInt)
 		val layered = layers(imageData, 25, 6)
 		val targetLayer = layered.minBy((layer: List[Int]) => layer.count(_ == 0))
 		println(targetLayer.count(_ == 1) * targetLayer.count(_ == 2))
+
+		applyLayers(imageData, 25, 6).grouped(25).foreach(println(_))
+		// ZLJBF
 	}
 }
--- src/test/scala/Day8Test.scala
+++ src/test/scala/Day8Test.scala
@@ -4,4 +4,7 @@ class Day8Test extends FunSuite {
 	test("Day8.layers") {
 		assert(Day8.layers(List(1,2,3,4,5,6,7,8,9,0,1,2), 3, 2) == List(List(1,2,3,4,5,6), List(7,8,9,0,1,2)))
 	}
+	test("Day8.applyLayers") {
+		assert(Day8.applyLayers(List(0,2,2,2,1,1,2,2,2,2,1,2,0,0,0,0), 2, 2) == List(0,1,1,0))
+	}
 }
